0bfc61d36ae45bf8e38774ae0d57afbe2b3750b6
David Zheng <zhengdav@msu.edu>
2020-02-26 13:43:26 -0800

    not-found page works

--- 2-25-20/src/app.js
+++ 2-25-20/src/app.js
@@ -3,11 +3,31 @@ const app = express();
 
 console.log(__dirname);
 
+// Body Parser Middleware
+app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+// Setting up EJS 
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/views`)
+
+// Routes
 app.use('/', express.static(`${__dirname}/../public/`));
 
 app.get('/', (request, response) => {
   response.send('<h1>Welcome to My Warmup</h1>')
 })
+
+app.get('*', (req, res) => {
+  res.status(404);
+  res.statusMessage = 'Not Found';
+  res.render('not-found', {request:req})
+})
+
+app.post('/save', (request, response) => {
+  response.send(request.body);
+})
+
 
 
 module.exports = {
