7cc9c0e5307e980487e0046fee4fa1bfd4e2ba81
David Zheng <zhengdav@msu.edu>
2020-03-28 20:21:38 -0700

    added routes and 404 message

--- 3-28-20/index.js
+++ 3-28-20/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
 
 const app = express();
+
+
+
+// Init middleware
+// app.use(logger);
+
+// Get all members 
+app.get('/api/members', (req, res) => res.json(members));
+
+
+// Get single member 
+app.get('/api/members/:id', (req, res) => {
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if(found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)))
+  } else {
+    res.status(400).json({msg: `No member with the id of ${req.params.id}`})
+  }
+
+});
+
+
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
