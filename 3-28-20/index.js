7fdef48973673b7b9ddfbedd6a3c6bbc1bf5bb56
David Zheng <zhengdav@msu.edu>
2020-03-29 14:51:25 -0700

    title renders using handlebars

--- 3-28-20/index.js
+++ 3-28-20/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 const app = express();
 const router = require('./routes/api/members');
@@ -9,13 +10,26 @@ const router = require('./routes/api/members');
 // Init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// HomePage Route
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Member App'
+  });
+})
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
 
-// set static folder
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Members API Routes 
